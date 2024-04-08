a1ad526732185a0edd1aece51a11c9d928dd4c0a
montenok <montenok-d@yandex.ru>
2024-04-08 22:17:18 +0300

    Edit format

--- src/TaskTimeException.java
+++ src/TaskTimeException.java
@@ -1,5 +1,5 @@
-public class TaskTimeException extends RuntimeException{
-        public TaskTimeException(final String message) {
-            super(message);
-        }
+public class TaskTimeException extends RuntimeException {
+    public TaskTimeException(final String message) {
+        super(message);
+    }
 }
